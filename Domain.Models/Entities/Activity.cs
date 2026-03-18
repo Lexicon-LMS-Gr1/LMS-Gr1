901d30126c83bef7a2ed6773ba99e91d1abb75f7
dave95995 <danielv3234@outlook.com>
2026-03-18 12:22:08 +0100

    Add minimal LMS domain model

---
+++ Domain.Models/Entities/Activity.cs
@@ -0,0 +1,25 @@
+﻿namespace Domain.Models.Entities;
+
+public class Activity
+{
+	public int Id { get; set; }
+
+	public required string Name { get; set; }
+	public required string Description { get; set; }
+
+	public DateTime StartTime { get; set; }
+	public DateTime EndTime { get; set; }
+	public DateTime? DueDate { get; set; }
+
+	// Foreign Key to ActivityType
+	public int ActivityTypeId { get; set; }
+
+	// Foreign Key to Module
+	public int ModuleId { get; set; }
+
+	// Navigation Property that represents the relationship between Activity and ActivityType
+	public required ActivityType ActivityType { get; set; }
+
+	// Navigation Property that represents the relationship between Activity and Module
+	public required Module Module { get; set; }
+}
