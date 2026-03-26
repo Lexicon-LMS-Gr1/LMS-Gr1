65f6b0aff55c7c0f582f048a1721f0f027fdc9e0
Palina Boczkowska <palina.boczkowska@gmail.com>
2026-03-26 17:23:33 +0100

    Draft

--- LMS.Shared/DTOs/Course/ModuleDto.cs
+++ LMS.Shared/DTOs/Course/ModuleDto.cs
@@ -11,6 +11,7 @@ namespace LMS.Shared.DTOs.Course
         public string Description { get; set; } = "";
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
+        public int CourseId { get; set; }
 
         public List<ActivityDto> Activities { get; set; } = new();
     }
